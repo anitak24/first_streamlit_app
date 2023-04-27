4744b3a005376bfb6dad031d3e195e2e22a8aacf
anitak24 <131946445+anitak24@users.noreply.github.com>
2023-04-27 12:27:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parent new healthy dinner')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & omlet')
+streamlit.text('orange & strawberry Smoothi')
+streamlit.text('scrambled egg')
