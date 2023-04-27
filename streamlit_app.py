6bbf3214dd4e79574448d4652a302537845154b4
anitak24 <131946445+anitak24@users.noreply.github.com>
2023-04-27 12:33:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & omlet')
 streamlit.text('orange & strawberry Smoothi')
 streamlit.text('scrambled egg')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
